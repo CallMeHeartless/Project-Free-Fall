f82239b45644663baf2d35a6c2855e1af414751b
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-02-19 12:15:04 +1300

    falling platform (press P)

---
+++ Project Free Fall/Assets/Drop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour
+{
+    public char lettter;
+    public int timer;
+    bool destoying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            destoying = true;
+        }
+        if (destoying == true)
+        {
+            if (timer !=0)
+            {
+                timer--;
+                gameObject.transform.Translate(0, -0.1f, 0);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
