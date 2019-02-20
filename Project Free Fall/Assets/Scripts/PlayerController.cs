d8dee968274a740fa2f03a8bdf4c1900c4057717
Kerry <k.of.blades@gmail.com>
2019-02-21 12:24:10 +1300

    Further adjusted player controls to smooth input between frames. Added a basic jump

--- Project Free Fall/Assets/Scripts/PlayerController.cs
+++ Project Free Fall/Assets/Scripts/PlayerController.cs
@@ -10,25 +10,26 @@ public class PlayerController : MonoBehaviour
     private string playerLeftXAxis;
     private string playerLeftYAxis;
     private string playerRightXAxis;
+    private string playerAButton;
 
     // Player movement variables
     [SerializeField]
     private float moveSpeed;
     [SerializeField]
     private float turnSpeed;
+    [SerializeField]
+    private float jumpForce;
     private Rigidbody rb;
     private Vector3 movement;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         rb = GetComponent<Rigidbody>();
         AssignPlayerID(playerID); // Change this later
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         movement = new Vector3();
         MovementInput();
         RotatePlayer();
@@ -36,36 +37,40 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate() {
         MoveBody();
+        Jump();
     }
 
-    public void AssignPlayerID(int _playerID)
-    {
+    public void AssignPlayerID(int _playerID) {
         playerID = _playerID;
         Debug.Log("Assigned ID " + _playerID.ToString() + " to player");
         playerLeftXAxis = "Controller_" + _playerID.ToString() + "_Left_X_Axis";
         playerLeftYAxis = "Controller_" + _playerID.ToString() + "_Left_Y_Axis";
         playerRightXAxis = "Controller_" + _playerID.ToString() + "_Right_X_Axis";
+        playerAButton = "Controller_" + _playerID.ToString() + "_A";
 
     }
 
-    void MovementInput()
-    {
+    void MovementInput(){
         Vector3 right = transform.right * Input.GetAxis(playerLeftXAxis);
         Vector3 forward = transform.forward * Input.GetAxis(playerLeftYAxis);
         movement = right + forward;
     }
 
-    void MoveBody()
-    {
+    void MoveBody(){
         if(movement != Vector3.zero) {
             rb.MovePosition(transform.position + moveSpeed * movement * Time.fixedDeltaTime);
         }
 
     }
 
-    void RotatePlayer()
-    {
-        transform.Rotate(transform.up, turnSpeed * Input.GetAxis(playerRightXAxis));
+    void RotatePlayer(){
+        transform.Rotate(transform.up, turnSpeed * Input.GetAxis(playerRightXAxis) * Time.deltaTime);
+    }
+
+    void Jump() {
+        if (Input.GetButtonDown(playerAButton) && rb.velocity.y == 0) {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     public void AddImpulse(Vector3 impulse) {
