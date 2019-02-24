dd6d42f346518bba196e952ed09fe3539b9311ce
Kerry <k.of.blades@gmail.com>
2019-02-25 09:23:27 +1300

    Tightened player dash, player should now stop at end of animation

---
+++ Project Free Fall/Assets/Scripts/PlayerAnimationsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationsController : MonoBehaviour
+{
+
+    private Transform dashBox;
+
+    private void Start() {
+        dashBox = transform.parent.Find("DashHitBox");
+    }
+
+    public void DashHitboxOn() {
+        dashBox.gameObject.SetActive(true);
+    }
+
+
+    public void DashHitboxOff() {
+        dashBox.gameObject.SetActive(false);
+        //Debug.Log("hitbox off");
+        transform.parent.GetComponent<PlayerController>().StopPlayer();
+    }
+}
