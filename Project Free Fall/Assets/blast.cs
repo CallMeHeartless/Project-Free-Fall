21912301a99bff2622771d8b14c1aed49c5628f8
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-02-25 12:11:24 +1300

    traps part 1

---
+++ Project Free Fall/Assets/blast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blast : MonoBehaviour
+{
+    public float Range;
+    public float speed;
+    public float thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<SphereCollider>().radius<= Range)
+        {
+            gameObject.GetComponent<SphereCollider>().radius += speed;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+      
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hit");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //gameObject.GetComponent<MeshCollider>().enabled = false;
+            collision.rigidbody.velocity = (transform.forward * thrust) + new Vector3(0, Random.Range(0.5f, 1.0f), 0);
+        }
+
+    }
+}
---
+++ Project Free Fall/Assets/blastTrap.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blastTrap : MonoBehaviour
+{
+    public float timer;
+    public bool On;
+    public float cooldown;
+    public float Range;
+    public float speed;
+    public float thrust;
+    public GameObject player;
+    public Transform prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            if (On == true)
+            {
+
+
+                if (timer >= 0)
+                {
+                    timer -= Time.deltaTime;
+
+
+                }
+                else
+                {
+                    //gameObject.GetComponent<MeshCollider>().enabled = true;
+                    On = true;
+                    timer = cooldown;
+                    Instantiate(prefab, transform.position, Quaternion.identity);
+               // prefab.transform.parent = gameObject.transform;
+                    prefab.GetComponent<blast>().speed = speed;
+                    prefab.GetComponent<blast>().Range = Range;
+                    prefab.GetComponent<blast>().thrust = thrust;
+            }
+            }
+           
+        }
+    
+}
